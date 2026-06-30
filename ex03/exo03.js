34e717e4d8dd67aad2270d424682f9cf1eae82e7
samambaiabr <teamomae231@gmail.com>
2026-06-30 19:10:44 -0300

    exer03

--- ex03/exo03.js
+++ ex03/exo03.js
@@ -1,59 +1,60 @@
-// const fromEx02 = document.getElementById("fromEx02");
+const fromEx03 = document.getElementById("fromEx03");
 
-// fromEx02.addEventListener("submit", function (evento) {
+fromEx03.addEventListener("submit", function (evento) {
 
+
+    evento.preventDefault();
+
+
+    const titulo = document.getElementById("titulo");
+    const autor = document.getElementById("autor");
+    const ano = document.getElementById("ano");
+    const genero = document.getElementById("genero");
+
+    if (titulo.value.trim() == "") {
+
+        titulo.classList.add("is-invalid")
+        titulo.classList.remove("is-valid")
+    } else {
+
+        titulo.classList.add("is-valid")
+        titulo.classList.remove("is-invalid")
+    }
+    if (autor.value.trim() == "") {
+
+        autor.classList.add("is-invalid")
+        autor.classList.remove("is-valid")
+    } else {
+
+        ano.classList.add("is-valid");
+        ano.classList.remove("is-invalid")
+    }
+    if (ano.value.trim() == "") {
+
+        ano.classList.add("is-invalid")
+        ano.classList.remove("is-valid")
+    } else {
+
+        ano.classList.add("is-valid");
+        ano.classList.remove("is-invalid")
+    }
+
+    console.log("Cliente " + Cliente.value);
+    console.log("Pizza " + Pizza.value);
+    console.log("Pizza " + Pizza.value);
 
     
-//     evento.preventDefault();
+    if (genero.value == "") {
+
+        genero.classList.add("is-invalid")
+        genero.classList.remove("is-valid")
+    } else {
+
+        genero.classList.add("is-valid");
+        genero.classList.remove("is-invalid")
+    }
+
     
+    console.log("Bebidas " + Bebidas.value);
 
-//     const Cliente = document.getElementById("Cliente");
-//     const Pizza = document.getElementById("Pizza");
-//     const Tamanho = document.getElementById("Tamamho");
-//     const Bebidas = document.getElementById("Bebidas");
-
-//     if (Cliente.value.trim() == "") {
-
-//         Cliente.classList.add("is-invalid")
-//         Cliente.classList.remove("is-valid")
-//     } else {
-
-//         Cliente.classList.add("is-valid")
-//         Cliente.classList.remove("is-invalid")
-//     }
-//     if (Pizza.value.trim() == "") {
-
-//         Pizza.classList.add("is-invalid")
-//         Pizza.classList.remove("is-valid")
-//     } else {
-
-//         Pizza.classList.add("is-valid");
-//         Pizza.classList.remove("is-invalid")
-//     }
-
-//     console.log("Cliente " + Cliente.value);
-//     console.log("Pizza " + Pizza.value);
-
-//     if (Tamanho.value == "") {
-
-//         Tamanho.classList.add("is-invalid")
-//         Tamanho.classList.remove("is-valid")
-//     } else {
-
-//         Tamanho.classList.add("is-valid")
-//         Tamanho.classList.remove("is-invalid")
-//     }
-//     if (Bebidas.value == "") {
-
-//         Bebidas.classList.add("is-invalid")
-//         Bebidas.classList.remove("is-valid")
-//     } else {
-
-//         Bebidas.classList.add("is-valid");
-//         Bebidas.classList.remove("is-invalid")
-//     }
-
-//     console.log("Tamanho " + Tamanho.value);
-//     console.log("Bebidas " + Bebidas.value);
-
-// });
+});
